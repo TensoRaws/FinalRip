68c2b04be7b74e5a9b67e1314115f38345ddf09b
Tohrusky <65994850+Tohrusky@users.noreply.github.com>
2024-08-03 02:26:20 +0800

    fix: mongodb connect

--- module/db/db.go
+++ module/db/db.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/TensoRaws/FinalRip/module/config"
 	"github.com/TensoRaws/FinalRip/module/log"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -33,18 +32,18 @@ func initialize() {
 	log.Logger.Info("Connecting to MongoDB: " + applyURI)
 	clientOpts := options.Client().ApplyURI(applyURI).SetAuth(credential)
 
-	cilent, err := mongo.Connect(context.TODO(), clientOpts)
+	client, err := mongo.Connect(context.TODO(), clientOpts)
 	if err != nil {
 		log.Logger.Error("Failed to connect to MongoDB: " + err.Error())
 		panic(err)
 	}
 
 	// Send a ping to confirm a successful connection
-	var result bson.M
-	if err = cilent.Database("finalrip").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Decode(&result); err != nil { //nolint
-		log.Logger.Error("Failed to connect to MongoDB: " + err.Error())
-		panic(err)
-	}
+	//var result bson.M
+	//if err = client.Database("finalrip").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Decode(&result); err != nil { //nolint
+	//	log.Logger.Error("Failed to connect to MongoDB: " + err.Error())
+	//	panic(err)
+	//}
 
-	DB = cilent.Database("finalrip")
+	DB = client.Database("finalrip")
 }
