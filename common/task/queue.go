b2a95edfc993368cce6f2591fbe8fff6219f99ab
Tohrusky <65994850+Tohrusky@users.noreply.github.com>
2024-12-23 07:27:46 +0000

    feat: support priority queue

---
+++ common/task/queue.go
@@ -0,0 +1,19 @@
+package task
+
+import "github.com/TensoRaws/FinalRip/module/queue"
+
+// GetQueueName 获取队列名称，如果没有则返回默认队列（低优先级）
+func GetQueueName(name *string) string {
+	if name == nil {
+		return queue.ENCODE_QUEUE_DEFAULT
+	}
+
+	switch *name {
+	case "default":
+		return queue.ENCODE_QUEUE_DEFAULT
+	case "priority":
+		return queue.ENCODE_QUEUE_PRIORITY
+	default:
+		return queue.ENCODE_QUEUE_DEFAULT
+	}
+}
